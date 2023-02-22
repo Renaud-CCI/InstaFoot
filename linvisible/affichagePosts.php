5e329cda61f897889abb42f811cc224c04678605
Renaud-CCI <120473987+Renaud-CCI@users.noreply.github.com>
2023-02-22 13:26:17 +0100

    Add files via upload

--- linvisible/affichagePosts.php
+++ linvisible/affichagePosts.php
@@ -11,33 +11,54 @@ function affichagePost($photoId){
     } catch (Exception $message) {
     echo "il y a un souci <br>" . "<pre>$message</pre>";
     }
+    //-------Création d'une liste d'id à afficher
+    $usersIds = [$_SESSION['id'],];
+
+    $req = $bdd->prepare("  SELECT abonnements.idAbonnement FROM abonnements
+                            WHERE idAbonne = :idAbonne");
+    $req->execute([ 'idAbonne' => $_SESSION['id']]);
+    $usersIdsReq = $req->fetchAll();
+    foreach($usersIdsReq as $userIdReq){
+        $usersIds[] = $userIdReq['idAbonnement'];
+    }
+    $usersIdsStr = implode(',',$usersIds);
+    // var_dump($usersIds);
+    // echo"<br>";
+    // var_dump($usersIdsStr);
+    // die;
 
     // ------Preparation des variables à afficher
     $req = $bdd->prepare("  SELECT * FROM photos
                             JOIN users ON users.id = photos.idUser
-                            WHERE photos.id = :photoId");
-    $req->execute([ 'photoId' => $photoId]);
-    $postToEcho = $req->fetch();
+                            WHERE photos.idUser IN ($usersIdsStr)
+                            ORDER BY date DESC");
+    $req->execute();
+    $postsToEcho = $req->fetchAll();
+
+    
 
     // -----Affichage du post----
-    echo "
-    <div class='postDiv w-50 container rounded bg-light'>
-    <div class='row pseudoRow'>
-        <form action='./profil.php' method='get'>
-            <input type='hidden' name='id' value={$postToEcho['idUser']}>
-            <button id='photoProfileButton' type='submit' class='d-flex p-2'>
-                <img class='photoProfile' src='{$postToEcho['photoProfile']}' alt='photo de profil'> 
-                <p>{$postToEcho['pseudo']}</p>
-            </button>
-        </form>
-    </div>
-    <div class='row photoRow'>
-        <img class='postPhoto' src='{$postToEcho['path']}' alt=''>
-    </div>
-    <div class='row postRow'>
-        {$postToEcho['post']}
-    </div>
-</div>
-    ";
+    foreach ($postsToEcho as $postToEcho){
+
+        echo "
+        <div class='postDiv w-50 container rounded bg-light'>
+            <div class='row pseudoRow'>
+                <form action='./profil.php' method='get'>
+                    <input type='hidden' name='id' value={$postToEcho['idUser']}>
+                    <button id='photoProfileButton' type='submit' class='d-flex p-2'>
+                        <img class='photoProfile' src='{$postToEcho['photoProfile']}' alt='photo de profil'> 
+                        <p>{$postToEcho['pseudo']}</p>
+                    </button>
+                </form>
+            </div>
+            <div class='row photoRow'>
+                <img class='postPhoto' src='{$postToEcho['path']}' alt=''>
+            </div>
+            <div class='row postRow'>
+                {$postToEcho['post']}
+            </div>
+        </div>
+        ";
+    }
 }
 ?>
