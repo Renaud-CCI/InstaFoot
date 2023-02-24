bceb2ced7d286ecd24c8a7612fb2c5ac64f78959
mendof <107985100+mendof@users.noreply.github.com>
2023-02-24 09:25:38 +0100

    Add files via upload

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>insta-foot</title>
+    <title>Insta Foot</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="./login.css">
   </head>
@@ -14,11 +14,11 @@
     unset($_SESSION);
 
     if ( isset ($_GET['action' ])){
-      $placeholder='pseudo non existant ';
+      $placeholder='Pseudo inconnu';
 
     }
     else 
-    $placeholder="saisie ton pseudo  "
+    $placeholder="Saisis ton pseudo  "
     ?>
   
 
